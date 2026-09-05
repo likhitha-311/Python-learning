afdfd204ed460176f13c97a697a4f589c177c4a7
likitha <thupililikitha3@gmail.com>
2026-09-05 20:52:32 +0530

    Add greeting function with user details

---
+++ Day4_greet.py
@@ -0,0 +1,12 @@
+# Day 4 Problem 1
+# Name: Likhitha | CAI Student
+# Date: 05-09-2026
+
+def greet(name, college, goal):
+    print(f"Hello, I am {name}")
+    print(f"Studying at {college}")
+    print(f"My goal is to become {goal}")
+
+# My details
+greet("Likhitha", "Svcn", "GenAI Engineer")
+print("Day 4 P1 Completed")
